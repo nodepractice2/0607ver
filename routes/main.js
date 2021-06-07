b2249b8b8b3069034d1b0eed608c46792db893fa
eelqkf <84161235+eelqkf@users.noreply.github.com>
2021-06-07 15:48:43 +0900

    Add files via upload

---
+++ routes/main.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const user = require('../routes/auth');
+// 데이터 값 연결 user.nick과 user.email로 로그인한 사용자 데이터 불러오기 가능
+const post = require('../models/post');
+//const jowon = require('../models/jowon');
+const { User } = require('../models');
+
+const router = express.Router();
+
+var postdata;  
+
+router.get('/', (req, res) => {
+  res.render('main');                               
+  });
+
+router.get('/QnA/write' ,(req,res)=>{
+
+  res.render('write');
+})
+
+
+// jowon db id pk, name, email......
+router.get('/Teammates', (req,res) =>{
+  res.render('Teammates');
+})
+
+router.post('/Teammates', async(req,res)=> {
+  var id =req.body.id;
+  await post.findOne({raw : true})
+  .then((result) =>{
+    console.log(result);
+    var data =result.content + result.writer;
+    console.log(data);
+    res.send(data);
+  })
+
+})
+  
+router.get('/post', async(req,res) => {
+          await post.findAll({raw : true})
+          .then((results) =>{
+            console.log(results);
+            postdata = results;
+          }).catch ((err)=>{
+           console.error(err);
+          });
+          res.redirect('/main/QnA');
+        });
+       
+router.get('/QnA' ,(req,res) => {
+  res.render('QnA' , {postdata});
+})                       
+
+router.post('/QnA/write' , async (req,res)=>{  //insert
+  const { content, postTiltle} = req.body;
+ 
+ try{
+  await post.create({
+    content,
+    postTiltle,
+    writer : user.nick,
+      });
+  return res.redirect('/main/post');
+ }catch (error) {
+  console.error(error);
+  return next(error);
+}
+});
+
+  module.exports = router;
